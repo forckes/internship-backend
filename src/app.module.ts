757c22496c680da388da660bd414d133ab04ea93
forckes <forckes@proton.me>
2023-10-20 18:52:33 +0300

    update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
+import { DealModule } from './deal/deal.module';
 
 @Module({
-	imports: [AuthModule, UserModule],
+	imports: [AuthModule, UserModule, DealModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
