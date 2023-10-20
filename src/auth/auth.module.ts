54373a0c1d4eda8de397e6976d9a9148a386a281
forckes <forckes@proton.me>
2023-10-20 16:03:41 +0300

    auth.module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { PrismaService } from 'src/prisma.service'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { getJwtConfig } from 'src/config/jwt.config'
+import { JwtStrategy } from './jwt.strategy'
+import { UserService } from 'src/user/user.service'
+import { UserModule } from 'src/user/user.module'
+
+@Module({
+	controllers: [AuthController],
+	providers: [AuthService, JwtStrategy, UserService, PrismaService],
+	imports: [
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		}),
+		UserModule
+	]
+})
+export class AuthModule {}
