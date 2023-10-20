2a5bb1303935cb1c5a33b96f9ada463f79ad5877
forckes <forckes@proton.me>
2023-10-20 15:01:25 +0300

    setuping required configurations

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
 	getHello(): string {
-		return 'Hello World!'
+		return 'Internship Test Task Backend. Created by forckes(Taras Lytvynenko)'
 	}
 }
