764faa5282bfc8692cb97cc70de6b78766b786c1
forckes <forckes@proton.me>
2023-10-20 16:00:38 +0300

    add jwt.strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { PassportStrategy } from '@nestjs/passport'
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PrismaService } from 'src/prisma.service'
+import { User } from '@prisma/client'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		private configService: ConfigService,
+		private prisma: PrismaService
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: true,
+			secretOrKey: configService.get('JWT_SECRET')
+		})
+	}
+
+	async validate({ id }: Pick<User, 'id'>) {
+		return this.prisma.user.findUnique({
+			where: {
+				id: +id
+			}
+		})
+	}
+}
