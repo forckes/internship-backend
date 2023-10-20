64fcf7a7c903cc3b5316db497bcda34f641ea110
forckes <forckes@proton.me>
2023-10-20 20:22:39 +0300

    add features for uploads and deals

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
-import { DealModule } from './deal/deal.module';
+import { DealModule } from './deal/deal.module'
+import { ConfigModule } from '@nestjs/config'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import * as appRootPath from 'app-root-path'
 
 @Module({
-	imports: [AuthModule, UserModule, DealModule],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${appRootPath}/uploads`,
+			serveRoot: '/uploads'
+		}),
+		ConfigModule.forRoot(),
+
+		AuthModule,
+		UserModule,
+		DealModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
