4bd79a1b09821e60dbe1cd7e6f84ad34b7584862
forckes <forckes@proton.me>
2023-10-20 15:40:58 +0300

    add user.controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	Patch,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import { UserDto } from './user.dto'
+
+@Controller('user')
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') id: number) {
+		return this.userService.byId(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Auth()
+	@HttpCode(200)
+	@Put('profile')
+	async getNewTokens(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+		return this.userService.updateProfile(id, dto)
+	}
+
+	@Auth()
+	@HttpCode(200)
+	@Patch('profile/favorites/:dealId')
+	async toggleFavorite(
+		@Param('dealId') dealId: string,
+		@CurrentUser('id') id: number
+	) {
+		return this.userService.toggleFavorite(id, +dealId)
+	}
+}
